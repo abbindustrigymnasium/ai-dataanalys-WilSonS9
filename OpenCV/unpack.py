2b244fc9b75be3ba5725a39efbf3ec53c9bd3154
WilSonS9 <54662723+WilSonS9@users.noreply.github.com>
2021-05-31 16:28:17 +0200

    Labeled images

--- OpenCV/unpack.py
+++ OpenCV/unpack.py
@@ -1,21 +1,18 @@
 # Script to simplify the file structure of a folder of folders
-# Instead of a folder of folders of folders with files, you move all the files up a step, giving you a folder of folders with files
+# Instead of a folder of folders with files, you get a folder with files
 
 import os
 import shutil
 
-names = os.listdir('./legos/')
+names = os.listdir('./images/')
 # names.remove('.DS_Store')
 
-for name in names:
-    p = './legos/' + name
-    subs = os.listdir(p)
-    i = 1
-    for sub in subs:
-        if os.path.isdir(p + '/' + sub):
-            files = os.listdir(p + '/' + sub)
-            for f in files:
-                shutil.move(p + '/' + sub + '/' + f,  p)
-                os.rename(p + '/' + f, p + '/' + str(i) + '.JPEG')
-                i += 1
-            shutil.rmtree(p + '/' + sub)
+i = 1
+for f in names:
+    p = './images/' + name
+    if os.path.isdir(p):
+        files = os.listdir(p)
+        for f in files:
+            os.rename(p + '/' + f, './images/' + str(i) + '.jpg')
+            i += 1
+        shutil.rmtree(p)
